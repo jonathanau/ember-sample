337f5794a1bb9c5d92f7f5edb78436b48159d9c2
jonathanau <jonathan.au@gmail.com>
2013-08-19 17:10:24 -0700

    Toggle Todo editing state

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -11,6 +11,13 @@ Todos.TodoController = Ember.ObjectController.extend({
       model.save();
       return value;
     }
-  }.property('model.isCompleted')
+  }.property('model.isCompleted'),
+
+ isEditing: false,
+
+ editTodo: function() {
+   this.set('isEditing', true);
+ }
+
 });
 
